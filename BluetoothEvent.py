34bec1d693ae5d1de87fdb75146d7100c6243e98
lostman-github <lostman.worlds.end+github@gmail.com>
2017-02-03 17:28:23 +0900

    select interval from menu

--- BluetoothEvent.py
+++ BluetoothEvent.py
@@ -5,7 +5,15 @@ import objc
 import urllib, urllib2
 import Foundation
 
-INTERVAL = 10 * 60
+INTERVALS = [
+    #("5 Seconds", 5),
+    ("1 Minutes", 1 * 60),
+    ("2 Minutes", 2 * 60),
+    ("5 Minutes", 5 * 60),
+    ("10 Minutes", 10 * 60),
+    ("20 Minutes", 20 * 60),
+    ("30 Minutes", 30 * 60),
+]
 
 def update_callback(name, address, state):
     pass
@@ -25,7 +33,7 @@ def update_callback(name, address, state):
     #    urllib.urlencode(payload)).read()
 
 class BluetoothEvent(rumps.App):
-    def __init__(self, interval, update_callback):
+    def __init__(self, intervals, update_callback):
         super(BluetoothEvent, self).__init__("BluetoothEvent")
 
         # Load IOBluetooth.framework
@@ -41,11 +49,29 @@ class BluetoothEvent(rumps.App):
         else:
             self.icon = "ic_phonelink_white_24dp.png"
 
+        # Setup timer
+        self.timer = None
+
+        self.menu.add(rumps.MenuItem("Intervals"))
+
+        self.intervals = {}
+        for title, value in intervals:
+            item = rumps.MenuItem(title, callback=self.interval_menu_callback)
+            self.intervals[title] = (
+                item,
+                value
+            )
+            self.menu.add(item)
+
+        self.menu.add(rumps.separator)
+
         # Prepare menu items for each devices
+        self.menu.add(rumps.MenuItem("Devices"))
+
         self.devices = {}
         for device in self.iobluetooth.IOBluetoothDevice.pairedDevices():
             self.devices[device.addressString()] = (
-                rumps.MenuItem(device.name(), callback=self.switch_callback),
+                rumps.MenuItem(device.name(), callback=self.device_menu_callback),
                 device.name(),
                 False
             )
@@ -55,18 +81,28 @@ class BluetoothEvent(rumps.App):
 
         self.menu.add(rumps.separator)
 
-        # Setup timer
-        self.timer = rumps.Timer(self.timer_callback, interval)
-        self.timer.start()
-
+        # Setup update callback
         self.update_callback = update_callback
 
-    def switch_callback(self, item):
+    def interval_menu_callback(self, item):
         item.state = not item.state
+
+        for title, value in filter(lambda x: x[0] != item.title, self.intervals.items()):
+            value[0].state = False
+
+        if self.timer and self.timer.is_alive():
+            self.timer.stop()
+
+        if item.state:
+            self.timer = rumps.Timer(self.timer_callback, self.intervals[item.title][1])
+            self.timer.start()
 
     def timer_callback(self, timer):
         for address, value in filter(lambda x: x[1][0].state, self.devices.items()):
             self.check(address)
+
+    def device_menu_callback(self, item):
+        item.state = not item.state
 
     def check(self, address):
         item, name, previous = self.devices[address]
@@ -82,6 +118,6 @@ class BluetoothEvent(rumps.App):
             self.update_callback(name, address, current)
 
 if __name__ == "__main__":
-    BluetoothEvent(INTERVAL, update_callback).run()
+    BluetoothEvent(INTERVALS, update_callback).run()
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
